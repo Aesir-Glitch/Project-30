c4194833c20e3db2fb46345a4360be7e56899e4c
Aesir-Glitch <65230467+Aesir-Glitch@users.noreply.github.com>
2020-08-21 20:01:16 -0500

    Add files via upload

---
+++ BlockClass.js
@@ -0,0 +1,16 @@
+class Block extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.Visibility = 255;
+  }
+
+ display(){
+   if(this.body.speed < 3){
+    super.display();
+   }
+   else{
+    World.remove(world, this.body);
+    this.Visibility = this.Visibility - 5;
+  }
+ }
+}
